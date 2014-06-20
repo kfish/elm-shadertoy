631471e08b017e85aad1e8069848f598a3a75e7a
Conrad Parker <conrad@metadecks.org>
2014-06-21 09:56:38 +1000

    move lockMessage to Main

--- src/Display.elm
+++ src/Display.elm
@@ -9,37 +9,18 @@ import Graphics.WebGL (..)
 import Model
 import Engine (..)
 
-import Shaders.WorldVertex (Vertex, worldVertex)
-import Shaders.Fire (fire)
-import Shaders.SimplePlasma (simplePlasma)
-import Shaders.VoronoiDistances (voronoiDistances)
-
 view : (Int,Int) -> Model.Person -> Mat4
 view (w,h) person =
     mul (makePerspective 45 (toFloat w / toFloat h) 0.01 100)
         (makeLookAt person.position (person.position `add` Model.direction person) j)
 
 scene : (Perception -> [Entity])
-    -> (Int,Int) -> Time -> Bool -> Model.Person -> Element
-scene entities (w,h) t isLocked person =
+    -> (Int,Int) -> Time -> Model.Person -> Element
+scene entities (w,h) t person =
   let
     p = { viewMatrix = view (w,h) person, globalTime = t, resolution = (w,h) }
   in
     layers [ color (rgb 135 206 235) (spacer w h)
            , webgl (w,h) (entities p)
-           , container w 140 (midLeftAt (absolute 40) (relative 0.5))
-                 (if isLocked then exitMsg else enterMsg)
            ]
-
-enterMsg : Element
-enterMsg = message "Click to go full screen and move your head with the mouse."
-
-exitMsg : Element
-exitMsg = message "Press <escape> to exit full screen."
-
-message : String -> Element
-message msg =
-   plainText <|
-    "This uses stuff that is only available in Chrome and Firefox!\n" ++
-    "\nPress arrows or WASD keys to move, space bar to jump.\n\n" ++ msg
 
--- src/Main.elm
+++ src/Main.elm
@@ -17,6 +17,18 @@ import Demo
 port movement : Signal (Int,Int)
 port isLocked : Signal Bool
 
+-- Ability to request and exit. Click screen to request lock. Press escape to
+-- give up the lock. This code can all be removed if you want to do this
+-- differently.
+
+port requestPointerLock : Signal ()
+port requestPointerLock =
+    dropWhen (lift2 (&&) Keyboard.shift isLocked) () Mouse.clicks
+
+port exitPointerLock : Signal ()
+port exitPointerLock =
+    always () <~ keepIf (any (\x -> x == 27)) [] Keyboard.keysDown
+
 -- Set up 3D world
 inputs : Signal Model.Inputs
 inputs =
@@ -34,16 +46,27 @@ main = world Demo.demoEntities
 world : Signal (Perception -> [Entity]) -> Signal Element
 world entities =
   let t = foldp (+) 0 (fps 60)
-  in  lift5 Display.scene entities Window.dimensions t isLocked person
+      wh = Window.dimensions
+  in 
+      lift3 lockMessage wh isLocked
+            (lift4 Display.scene entities wh t person)
 
--- Ability to request and exit. Click screen to request lock. Press escape to
--- give up the lock. This code can all be removed if you want to do this
--- differently.
+lockMessage : (Int,Int) -> Bool -> Element -> Element
+lockMessage (w,h) isLocked e =
+    layers [ e
+           , container w 140 (midLeftAt (absolute 40) (relative 0.5))
+                 (if isLocked then exitMsg else enterMsg)
+           ]
 
-port requestPointerLock : Signal ()
-port requestPointerLock =
-    dropWhen (lift2 (&&) Keyboard.shift isLocked) () Mouse.clicks
+enterMsg : Element
+enterMsg = message "Click to go full screen and move your head with the mouse."
 
-port exitPointerLock : Signal ()
-port exitPointerLock =
-    always () <~ keepIf (any (\x -> x == 27)) [] Keyboard.keysDown
+exitMsg : Element
+exitMsg = message "Press <escape> to exit full screen."
+
+message : String -> Element
+message msg =
+   plainText <|
+    "This uses stuff that is only available in Chrome and Firefox!\n" ++
+    "\nPress arrows or WASD keys to move, space bar to jump.\n\n" ++ msg
+
