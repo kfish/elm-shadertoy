885191afedc2190d44b9b6080c2df691f2bc6e75
Conrad Parker <conrad@metadecks.org>
2016-03-15 04:33:03 +1100

    Dualscreen: split screen when 2nd gamepad connected
    Also, remap the Rock Candy to a standard gamepad

--- src/Main.elm
+++ src/Main.elm
@@ -11,7 +11,9 @@ import Char exposing (toCode)
 import Graphics.Element exposing (..)
 import Random
 import Set
-import Signal exposing (..)
+import Signal exposing (dropRepeats, sampleOn, merge)
+import Signal.Extra exposing (combine)
+import String exposing (contains)
 import Text
 import Time exposing (Time, fps)
 
@@ -43,33 +45,35 @@ port isLocked : Signal Bool
 
 port requestPointerLock : Signal ()
 port requestPointerLock =
-    let dropWhen bs def sig = map snd (filter fst (False, def) (map2 (,) (sampleOn sig (map not bs)) sig)) in
-    dropWhen (map2 (&&) Keyboard.shift isLocked) () Mouse.clicks
+    let dropWhen bs def sig = Signal.map snd
+            (Signal.filter fst (False, def) (Signal.map2 (,) (Signal.sampleOn sig (Signal.map not bs)) sig))
+    in
+    dropWhen (Signal.map2 (&&) Keyboard.shift isLocked) () Mouse.clicks
 
 
 port exitPointerLock : Signal ()
 port exitPointerLock =
-    map (always ()) (filter (Set.member 27) Set.empty Keyboard.keysDown)
+    Signal.map (always ()) (Signal.filter (Set.member 27) Set.empty Keyboard.keysDown)
 
 ----------------------------------------------------------------------
 -- Gamepad
 
-gamepadsToArrows : List Gamepad.Gamepad -> { x : Float, y : Float, mx : Float, my : Float }
-gamepadsToArrows gamepads =
+gamepadToArrows : Gamepad.Gamepad -> { x : Float, y : Float, mx : Float, my : Float }
+gamepadToArrows gamepad =
     let
         sgn x = if x < 0 then -1 else 1
         -- Apply a deadzone of size l around an axis centered at 0
         u l x = if abs x < l then 0.0 else sgn x * (abs x - l) / (1.0 - l)
         deadzone = u 0.2
 
+{-
         -- Map a trigger to a standard range [0..1], applying a deadzone
         dc l x = let xm = (x+1.0)/2.0 in u l xm
         toTrigger = dc 0.2
+-}
 
-        axs = case List.head gamepads of
-            Nothing -> {x=0, y=0, mx=0, my=0 }
-            Just gamepad ->
-              case gamepad.axes of
+        axs = case gamepad.axes of
+{-
                   -- Performance Designed Products Rock Candy Gamepad for Xbox 360 (Vendor: 0e6f Product: 011f)
                   [x1, y1, b1, x2, y2, b2, x3, y3] ->
                       { x = deadzone x1 / 10
@@ -77,6 +81,7 @@ gamepadsToArrows gamepads =
                       , mx = deadzone x2 /20
                       , my = deadzone (-1.0 * y2) / 20
                       }
+-}
 
                   -- ©Microsoft Corporation Controller (STANDARD GAMEPAD Vendor: 045e Product: 028e)
                   [x1, y1, x2, y2] ->
@@ -93,44 +98,65 @@ gamepadsToArrows gamepads =
                       {x = 0, y = 0, mx = 0, my = 0 }
 
         -- Interpret brake, accelerator as y input
-        btns_y = case List.head gamepads of
-            Nothing -> 0
-            Just gamepad ->
-                case gamepad.buttons of
+        btns_y = case gamepad.buttons of
                     -- ©Microsoft Corporation Controller (STANDARD GAMEPAD Vendor: 045e Product: 028e)
                     [a, b, x, y, lt, rt, l, r, back, start, lstick, rstick, padU, padD, padL, padR, logo] -> r.value - l.value
 
+{-
                     -- Performance Designed Products Rock Candy Gamepad for Xbox 360 (Vendor: 0e6f Product: 011f)
                     [a, b, x, y, lt, rt, back, start, logo, lstick, rstick] -> 0
+-}
 
                     _ -> 0
 
     in
         { axs | y = btns_y + axs.y }
 
+gamepadsToArrows : List Gamepad.Gamepad -> List { x : Float, y : Float, mx : Float, my : Float }
+gamepadsToArrows = List.map gamepadToArrows
+
 gamepadNothing : Gamepad.Gamepad
 gamepadNothing = { id = "", axes = [], buttons = [], mapping = "" }
 
--- Only set each button .pressed on a rising edge, ie. when .pressed transitions from False to True.
--- Preserves button .value
-gamepadEdge : Gamepad.Gamepad -> Gamepad.Gamepad -> Gamepad.Gamepad
-gamepadEdge prev now =
-    let d p n = if (n.pressed && not p.pressed) then n else { n | pressed = False }
-        newlyPressed = List.map2 d prev.buttons now.buttons
-    in { now | buttons = newlyPressed }
+toStandardGamepad : Gamepad.Gamepad -> Gamepad.Gamepad
+toStandardGamepad gamepad =
+    if gamepad.id `contains` "STANDARD GAMEPAD" then
+        gamepad
+    else
+        let (axes', buttons') = case (gamepad.axes, gamepad.buttons) of
+            -- Performance Designed Products Rock Candy Gamepad for Xbox 360 (Vendor: 0e6f Product: 011f)
+            ( [x1, y1, b1, x2, y2, b2, x3, y3]
+            , [a, b, x, y, lt, rt, back, start, logo, lstick, rstick]) ->
+                let
+                    toTrigger b = let b' = (b+1.0)/2.0 in
+                        if b' > 0 then
+                            { pressed = True, value = b' }
+                        else
+                            { pressed = False, value = 0 }
+                    b1' = toTrigger b1
+                    b2' = toTrigger b2
 
-gamepadsPressed : Signal (List Gamepad.Gamepad)
-gamepadsPressed =
-    let
-        -- Like List.map2, but ensures that there are always as many
-        -- elements in the result as in the right list
-        map2Now f now prev =
-            case (prev, now) of
-                (_      , []     ) -> []
-                ([]     , _      ) -> now
-                ((p::ps), (n::ns)) -> (f p n :: map2Now f ps ns)
-    in
-        foldp (map2Now gamepadEdge) [] Gamepad.gamepads
+                    toPads ax =
+                        if ax > 0 then
+                            ({ pressed = True, value = ax }, { pressed = False, value = 0 })
+                        else if ax < 0 then
+                            ({ pressed = False, value = 0 }, { pressed = True, value = -ax })
+                        else
+                            ({ pressed = False, value = 0 }, { pressed = False, value = 0 })
+                    (padL, padR) = toPads x3
+                    (padU, padD) = toPads y3
+
+                in
+                    ( [x1, y1, x2, y2]
+                    , [ a, b, x, y, lt, rt, b1', b2', back, start
+                      , lstick, rstick, padU, padD, padL, padR, logo]
+                    )
+
+            _ -> (gamepad.axes, gamepad.buttons)
+        in { gamepad | axes = axes', buttons = buttons' }
+
+standardGamepads : Signal (List Gamepad.Gamepad)
+standardGamepads = Signal.map (List.map toStandardGamepad) Gamepad.gamepads
 
 {- a b x y ltop rtop l r 
 l tiny (back), r tiny (start)
@@ -143,25 +169,28 @@ type alias GamepadButtons = { bA : Bool, bB : Bool, bX : Bool, bY : Bool, bBack 
 
 gamepadButtonsNone = { bA = False, bB = False, bX = False, bY = False, bBack = False, bStart = False }
 
-gamepadsToButtons : List Gamepad.Gamepad -> GamepadButtons
-gamepadsToButtons gamepads =
-    case List.head gamepads of
-        Nothing -> gamepadButtonsNone
-        Just gamepad ->
-            case gamepad.buttons of
-                (a::b::x::y::lt::rt::l::r::back::start::_) ->
-                    { bA = a.pressed, bB = b.pressed, bX = x.pressed, bY = y.pressed,
-                      bBack = back.pressed, bStart = start.pressed }
+gamepadToButtons : Gamepad.Gamepad -> GamepadButtons
+gamepadToButtons gamepad =
+    case gamepad.buttons of
+        (a::b::x::y::lt::rt::l::r::back::start::_) ->
+            { bA = a.pressed, bB = b.pressed, bX = x.pressed, bY = y.pressed,
+              bBack = back.pressed, bStart = start.pressed }
 
-                _ -> gamepadButtonsNone
+        _ -> gamepadButtonsNone
+
+gamepadsToButtons : List Gamepad.Gamepad -> List GamepadButtons
+gamepadsToButtons = List.map gamepadToButtons
 
 ----------------------------------------------------------------------
 
-gamepadsToInputs : List Gamepad.Gamepad -> Time -> Model.Inputs
-gamepadsToInputs gamepads dt =
-    let {x,y,mx,my} = gamepadsToArrows gamepads
-        {bA, bB, bX, bY, bBack, bStart} = gamepadsToButtons gamepads
+gamepadToInputs : Gamepad.Gamepad -> Time -> Model.Inputs
+gamepadToInputs gamepad dt =
+    let {x,y,mx,my} = gamepadToArrows gamepad
+        {bA, bB, bX, bY, bBack, bStart} = gamepadToButtons gamepad
     in  { noInput | reset = bStart, x = x, y = y, mx=mx, my=my, button_X = bX, dt = dt }
+
+gamepadsToInputs : List Gamepad.Gamepad -> Time -> List Model.Inputs
+gamepadsToInputs gamepads dt = List.map (\g -> gamepadToInputs g dt) gamepads
 
 mouseDeltas : Signal (Time, (Float, Float))
 mouseDeltas =
@@ -172,7 +201,7 @@ mouseDeltas =
 -- Set up 3D world
 kbMouseInputs : Signal Model.Inputs
 kbMouseInputs =
-  let dt = map Time.inSeconds (fps 60)
+  let dt = Signal.map Time.inSeconds (fps 60)
       dirKeys = merge Keyboard.arrows Keyboard.wasd
       yo x = x / 500
       handleKeys s bx {x,y} kdt = dbg_X
@@ -180,19 +209,41 @@ kbMouseInputs =
       dbg_X i = if i.button_X then Debug.log "Button X pressed" i else i
       x_key = Keyboard.isDown 88
   in  merge
-          (sampleOn dt <| map4 handleKeys Keyboard.space x_key dirKeys dt)
-          (map (\(mt, (mx,my)) -> { noInput | mx= yo mx, my= yo my, mt = mt }) mouseDeltas)
+          (sampleOn dt <| Signal.map4 handleKeys Keyboard.space x_key dirKeys dt)
+          (Signal.map (\(mt, (mx,my)) -> { noInput | mx= yo mx, my= yo my, mt = mt }) mouseDeltas)
 
-gamepadInputs : Signal Model.Inputs
+gamepadInputs : Signal (List Model.Inputs)
 gamepadInputs =
-  let dt = map Time.inSeconds (fps 60)
-  in  sampleOn dt <| map2 gamepadsToInputs gamepadsPressed dt
+  let dt = Signal.map Time.inSeconds (fps 60)
+  in  sampleOn dt <| Signal.map2 gamepadsToInputs standardGamepads dt
 
-inputs : Signal Model.Inputs
-inputs = merge (dropRepeats kbMouseInputs) (dropRepeats gamepadInputs)
+atWithDefault : a -> Int -> List a -> a
+atWithDefault def n list = Maybe.withDefault def (List.head (List.drop n list))
 
-person : Placement -> Array2D Float -> Signal Model.Person
-person placement terrain = foldp (Update.step placement terrain) Model.defaultPerson inputs
+gamepad1 : Signal Model.Inputs
+-- gamepad1 = Signal.map (Maybe.withDefault noInput << List.head) gamepadInputs
+gamepad1 = Signal.map (atWithDefault noInput 0) gamepadInputs
+
+gamepad2 : Signal Model.Inputs
+gamepad2 = Signal.map (atWithDefault noInput 1) gamepadInputs
+
+-- inputs : Signal Model.Inputs
+-- inputs = merge (dropRepeats kbMouseInputs) (dropRepeats gamepad1)
+
+inputs1 : Signal Model.Inputs
+inputs1 = merge (dropRepeats kbMouseInputs) (dropRepeats gamepad1)
+
+inputs2 : Signal Model.Inputs
+inputs2 = dropRepeats gamepad2
+
+-- person : Placement -> Array2D Float -> Signal Model.Person
+-- person placement terrain = Signal.foldp (Update.step placement terrain) Model.defaultPerson inputs
+
+person1 : Placement -> Array2D Float -> Signal Model.Person
+person1 placement terrain = Signal.foldp (Update.step placement terrain) Model.defaultPerson inputs1
+
+person2 : Placement -> Array2D Float -> Signal Model.Person
+person2 placement terrain = Signal.foldp (Update.step placement terrain) Model.defaultPerson inputs2
 
 {-| The main function -}
 main : Signal Element
@@ -200,17 +251,33 @@ main = world Demo.demoThings
 
 world : (Array2D Float -> Signal (List Thing)) -> Signal Element
 world thingsOnTerrain =
-  let t = foldp (+) 0 (fps 30)
-      wh = Window.dimensions
+  let t = Signal.foldp (+) 0 (fps 30)
+      wh  = Window.dimensions
+      wh2 = Signal.map (\(w,h) -> (w//2, h)) wh
 
       placement = defaultPlacement
 
       seed0 = Random.initialSeed 7777
       (terrain, seed1) = Random.generate (randTerrain2D (placement.bigSide+1)) seed0
       entities = thingsOnTerrain terrain
+
+      oneScene = Signal.map4 scene entities wh t (person1 placement terrain)
+      dualScene =
+            (Signal.map2 beside
+                (Signal.map4 scene entities wh2 t (person1 placement terrain))
+                (Signal.map4 scene entities wh2 t (person2 placement terrain)))
+
+      ifElse : (a -> Bool) -> b -> b -> a -> b
+      ifElse p ifBranch elseBranch x = if p x then ifBranch else elseBranch
+      chooseScene = Signal.map3 (ifElse (\l -> List.length l > 1)) dualScene oneScene Gamepad.gamepads
   in 
-      Signal.map3 lockMessage wh isLocked
-            (Signal.map4 scene entities wh t (person placement terrain))
+      -- Signal.map3 lockMessage wh isLocked
+      Signal.map2 debugLayer
+          (combine [Signal.map show Gamepad.gamepads, Signal.map show gamepadInputs])
+            chooseScene
+
+debugLayer : List Element -> Element -> Element
+debugLayer xs e = layers [ e, flow down xs ]
 
 lockMessage : (Int,Int) -> Bool -> Element -> Element
 lockMessage (w,h) isLocked e =
